c7c30a0decc74d5e2d0b96f36088370f9f4cee72
Shoormax <valentin.derudet2@gmail.com>
2017-02-03 22:52:31 +0100

    problème avec l'écriture des fichiers

--- classes/Rename.php
+++ classes/Rename.php
@@ -24,20 +24,20 @@ class Rename
         foreach ($files as $file) {
             $chemin = self::decomposeFichier($file)['chemin'];
             $t = file_get_contents($file);
-            $extension =  pathinfo($file, PATHINFO_EXTENSION);
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
 
             $tempnum = explode('ID', $t);
             $tempnum2 = explode('TIT2', $tempnum[1]);
 
             $number = '';
 
-            for($i = 0; $i < strlen($tempnum2[0]); $i++){
-                if(in_array($tempnum2[0][$i], self::$chiffre)) {
+            for ($i = 0; $i < strlen($tempnum2[0]); $i++) {
+                if (in_array($tempnum2[0][$i], self::$chiffre)) {
                     $number .= $tempnum2[0][$i];
                 }
             }
 
-            $number = strlen($number) == 1 ? '0'.$number.'.' : $number.'.';
+            $number = strlen($number) == 1 ? '0' . $number . '.' : $number . '.';
 
             $temp = explode('TIT2', $t);
 
@@ -45,17 +45,17 @@ class Rename
 
             $temp2 = explode('TPE1', $temp[1]);
 
-            for($j = 0; $j < strlen($temp2[0]); $j++){
-                if(ctype_alpha($temp2[0][$j]) || $temp2[0][$j] === " ") {
+            for ($j = 0; $j < strlen($temp2[0]); $j++) {
+                if (ctype_alpha($temp2[0][$j]) || $temp2[0][$j] === " ") {
                     $tempTitle .= $temp2[0][$j];
                 }
             }
 
-            $title = $number.' '.$tempTitle;
+            $title = $number . ' ' . $tempTitle;
 
             $title_end = preg_replace("/ /", "\ ", $title);
 
-            rename($file, $chemin.$title.'.'.$extension);
+            rename($file, $chemin . $title . '.' . $extension);
         }
     }
 
@@ -71,14 +71,14 @@ class Rename
         $array = explode('/', $file);
         $nom = array_pop($array);
 
-        $chemin='';
-        foreach($array as $ar) {
-            $chemin .= $ar.'/';
+        $chemin = '';
+        foreach ($array as $ar) {
+            $chemin .= $ar . '/';
         }
 
         return array(
-            'nom'       => $nom,
-            'chemin'    => $chemin
+            'nom' => $nom,
+            'chemin' => $chemin
         );
     }
 
@@ -93,81 +93,82 @@ class Rename
         $musiques = array();
         $dossiers = array();
 
-        if($parent != '') {
-            $parent = $parent.'/';
+        if ($parent != '') {
+            $parent = $parent . '/';
         }
 
         foreach ($files as $file) {
             $t = (explode('.', $file));
-            if(!in_array($file, self::$dossiersCaches)) {
-                if(in_array(array_pop($t), self::$extensions)) {
-                    $musiques[] = $parent.$file;
-                }
-                else {
-                    $dossiers[] = $parent.$file;
+            if (!in_array($file, self::$dossiersCaches)) {
+                if (in_array(array_pop($t), self::$extensions)) {
+                    $musiques[] = $parent . $file;
+                } else {
+                    $dossiers[] = $parent . $file;
                 }
             }
         }
-        if(empty($musiques) && empty($dossiers)) {
+        if (empty($musiques) && empty($dossiers)) {
             return false;
         }
         return array(
-            'musiques'  => $musiques,
-            'dossiers'  => $dossiers
+            'musiques' => $musiques,
+            'dossiers' => $dossiers
         );
     }
 
     public static function getSousDossiers($parent)
     {
         $dossiers = array();
-        if($parent != './') {
-            $parent = $parent.'/';
+        if ($parent != './') {
+            $parent = $parent . '/';
         }
         foreach (scandir($parent) as $fichier) {
-            if(strlen($fichier) > 2) {
-                if($fichier[0].$fichier[1] != '._' &&
+            if (strlen($fichier) > 2) {
+                if ($fichier[0] . $fichier[1] != '._' &&
                     !in_array($fichier, self::$dossiersCaches) &&
                     !in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$extensions) &&
-                    !in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$forbidenExtensions))
-                {
-                    $dossiers[] = $parent.$fichier;
+                    !in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$forbidenExtensions)
+                ) {
+                    $dossiers[] = $parent . $fichier;
                 }
             }
         }
         return $dossiers;
     }
 
-    public static function getMusiques($parent) {
+    public static function getMusiques($parent)
+    {
         $musiques = array();
-        if($parent != './') {
-            $parent = $parent.'/';
+        if ($parent != './') {
+            $parent = $parent . '/';
         }
         foreach (scandir($parent) as $fichier) {
-            if(!in_array($fichier, self::$dossiersCaches) &&
+            if (!in_array($fichier, self::$dossiersCaches) &&
                 in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$extensions) &&
-                !in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$forbidenExtensions))
-            {
-                $musiques[] = $parent.$fichier;
+                !in_array(pathinfo($fichier, PATHINFO_EXTENSION), self::$forbidenExtensions)
+            ) {
+                $musiques[] = $parent . $fichier;
             }
         }
 
         return $musiques;
     }
 
-    public static function getRealTitle($file) {
+    public static function getRealTitle($file)
+    {
         $props = file_get_contents($file);
-        $extension =  pathinfo($file, PATHINFO_EXTENSION);
-var_dump($props);
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        var_dump($props);
         $tempnum2 = explode('TIT2', $props)[1];
         var_dump($tempnum2);
-        if($tempnum2 != 'error') {
+        if ($tempnum2 != 'error') {
             $properties = array();
             $i = 0;
             foreach (explode($tempnum2[0], $tempnum2) as $property) {
 
-                if(!empty($property) && self::isValidChar($property[0])) {
+                if (!empty($property) && self::isValidChar($property[0])) {
                     //if($i < 19) {
-                        $properties[] = $property;
+                    $properties[] = $property;
 //                        $i ++;
 //                    }
 
@@ -199,7 +200,27 @@ var_dump($props);
         $str = str_replace('|', '', $str);
         $str = str_replace('?', '', $str);
         $str = str_replace('"', '', $str);
+        $str = str_replace('ç', 'c', $str);
+        $str = str_replace('à', 'a', $str);
+        $str = str_replace('â', 'a', $str);
+        $str = str_replace('ä', 'a', $str);
+        $str = str_replace('é', 'e', $str);
+        $str = str_replace('ë', 'e', $str);
+        $str = str_replace('è', 'e', $str);
+        $str = str_replace('ê', 'e', $str);
+        $str = str_replace('ô', 'o', $str);
+        $str = str_replace('ö', 'o', $str);
+        $str = str_replace('ø', 'o', $str);
+        $str = str_replace('ç', 'c', $str);
+        $str = str_replace('è', 'e', $str);
+        $str = str_replace('ñ', 'n', $str);
+        $str = str_replace('û', 'u', $str);
+        $str = str_replace('ü', 'u', $str);
+        $str = str_replace('ù', 'u', $str);
+        $str = str_replace('&amp;', '&', $str);
+
 
         return $str;
+
     }
 }
--- testVal/rename.php
+++ testVal/rename.php
@@ -1,23 +1,27 @@
 <?php
 set_time_limit(-1);
+setlocale (LC_ALL, "fr_FR");
+
 $ajd =  new DateTime('now');
 echo '<link rel="stylesheet" href="css.css">';
 echo $ajd->format('Y-m-d h:i:s').'<br>';
+
 require_once('../getid3/getid3.php');
 require_once('../classes/Rename.php');
 
 $extensions = array('.mp3', '.m4a', '.wav', '.mp4');
 $dossierArtisteAlbum = array();
+$musiques = array();
 $getID3 = new getID3;
+$separateur = 'separateur';
 
 if(isset($_POST['chemin']) && !empty($_POST['chemin']) && file_exists($_POST['chemin'])) {
     $parents = array($_POST['chemin']);
 }
 else {
-    die('Chemin non valide');
+    die('Chemin non valide.');
 }
 
-$musiques = array();
 for($i = 0; $i < 10; $i++) {
     foreach ($parents as $parent) {
         foreach (Rename::getSousDossiers($parent) as $sousDossier) {
@@ -31,6 +35,7 @@ for($i = 0; $i < 10; $i++) {
 foreach($parents as $parent) {
     foreach(Rename::getMusiques($parent) as $filename) {
         $ThisFileInfo = $getID3->analyze($filename);
+        $tempName = explode('.',$ThisFileInfo['filename'])[0];
 
         getid3_lib::CopyTagsToComments($ThisFileInfo);
 
@@ -47,61 +52,65 @@ foreach($parents as $parent) {
 
         if(in_array($extension, $extensions)) {
             $oldFullName = $ThisFileInfo['filenamepath'];
-            $titre_recup = empty($ThisFileInfo['comments']['title']) ? 'NoTitle' : $ThisFileInfo['comments']['title'][0];
+            $titre_recup = empty($ThisFileInfo['comments']['title']) ? 'NoTitle'.$tempName : $ThisFileInfo['comments']['title'][0];
+
             $titre_recup = Rename::replaceInvalidChar($titre_recup);
-            $titre = $parent.'/'.$titre_recup.$extension;
+            $titre = $parent.'/'.$titre_recup;
             $titre_duplicate = '';
 
             $i = 1;
-            while(file_exists($titre.$titre_duplicate)) {
-                $titre_duplicate .= ' ('.$i.')';
+            while(file_exists($titre.$titre_duplicate) || file_exists($titre.$titre_duplicate.$extension)) {
+                $titre_duplicate = ' ('.$i.')';
                 $i++;
             }
-
-            $titre = $titre.$titre_duplicate;
+            $titre = $titre.$titre_duplicate.$extension;
 
             $album_recup = empty($ThisFileInfo['comments']['album']) ? 'NoAlbum' : $ThisFileInfo['comments']['album'][0];
             $artist_recup = empty($ThisFileInfo['comments']['artist']) ? 'NoArtist' : $ThisFileInfo['comments']['artist'][0];
 
-            echo '<table class="array">';
-            echo '<tr><td>Titre duplicate</td><td>'.$titre.'</td></tr>';
-            $dossierArtisteAlbum[$artist_recup][$album_recup][] = str_replace($parent.'/', '',$titre);
-            echo '<tr><td>Titre</td><td>'. $titre_recup.'</td></tr>';
-            echo '<tr><td>Artiste</td><td>'.$artist_recup.'</td></tr>';
-            echo '<tr><td>Album</td><td>'.$album_recup.'</td></tr>';
-            echo '<table>';
-            echo '<hr>';
-            
-            //A sortir du grand foreach
-            foreach ($dossierArtisteAlbum as $i => $key) {
-                $dossierACreer = Rename::replaceInvalidChar($i);
-
-                foreach ($key as $j => $zic) {
-                    $dossierChildrenACreer = Rename::replaceInvalidChar($j);
-                    if(!file_exists($dossierACreer . '/' . $dossierChildrenACreer)) {
-                        mkdir($dossierACreer . '/' . $dossierChildrenACreer, 0777, true);
-                    }
-                    try {
-                        if(copy($oldFullName, $dossierACreer.'/'.$dossierChildrenACreer.'/'. str_replace($parent.'/', '',$titre))) {
-                            echo '<br>Success !<br>';
-                        }
-                        else {
-                            echo '<br>Failure !<br>';
-                            var_dump($oldFullName);
-                            var_dump($dossierACreer.'/'.$dossierChildrenACreer.'/'. str_replace($parent.'/', '',$titre));
-                        }
-                    }
-                    catch (Exception $e) {
-                        var_dump($oldFullName);
-                        var_dump($dossierACreer.'/'.$dossierChildrenACreer.'/'. str_replace($parent.'/', '',$titre));
-                        var_dump($e->getMessage());
-                    }
-                }
-//                die; //pour tester avec un seul fichier, décomenter cette ligne
+            if(isset($_POST['showInfos']) && !empty($_POST['showInfos']) && $_POST['showInfos']) {
+                echo '<table class="array">';
+                echo '<tr><td>Titre duplicate</td><td>'.$titre.'</td></tr>';
+                echo '<tr><td>Titre</td><td>'.$titre_recup.'</td></tr>';
+                echo '<tr><td>Titre écriture</td><td>'.str_replace($parent.'/', '',$titre).$separateur.$oldFullName.'</td></tr>';
+                echo '<tr><td>Artiste</td><td>'.$artist_recup.'</td></tr>';
+                echo '<tr><td>Album</td><td>'.$album_recup.'</td></tr>';
+                echo '<table>';
+                echo '<hr>';
             }
+
+            $dossierArtisteAlbum[$artist_recup][$album_recup][] = str_replace($parent.'/', '',$titre).$separateur.$oldFullName;
+            //die('<pre>'.htmlentities(print_r($dossierArtisteAlbum, true), ENT_SUBSTITUTE).'</pre>'); //On shot
+//            if(sizeof($dossierArtisteAlbum) > 50) {
+//                die('<pre>'.htmlentities(print_r($dossierArtisteAlbum, true), ENT_SUBSTITUTE).'</pre>');
+//                break 2;
+//            }
+//            break;
         }
         else {
             echo 'CAY UN DOSSIER : <pre>'.$filename.$extension.'</pre>';
+        }
+    }
+}
+
+foreach ($dossierArtisteAlbum as $i => $key) {
+    $dossierACreer = Rename::replaceInvalidChar($i);
+    foreach ($key as $j => $zics) {
+        foreach ($zics as $zic) {
+            $temp = explode($separateur, $zic);
+            $titre = $temp[0];
+            $oldFullName = $temp[1];
+            $dossierChildrenACreer = Rename::replaceInvalidChar($j);
+
+            if (!file_exists($dossierACreer . '/' . $dossierChildrenACreer)) {
+                mkdir($dossierACreer . '/' . $dossierChildrenACreer, 0777, true);
+            }
+
+            if (!copy($oldFullName, $dossierACreer . '/' . $dossierChildrenACreer . '/' . $titre)) {
+                echo '<br>Failure !<br>';
+                var_dump($oldFullName);
+                var_dump($dossierACreer . '/' . $dossierChildrenACreer . '/' . $titre);
+            }
         }
     }
 }
